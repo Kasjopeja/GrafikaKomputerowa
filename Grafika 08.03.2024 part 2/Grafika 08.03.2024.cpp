8ed323eafdbca2106013459d1e63803d13740041
Aleksandra <azakrecka@student.agh.edu.pl>
2024-03-08 22:10:56 +0100

    basic setup

---
+++ Grafika 08.03.2024 part 2/Grafika 08.03.2024.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Openglwindow", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" <<
+			std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport 
+	// of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+	// Specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+	// Vertex Shader source code
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	//Fragment Shader source code
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		" FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+		"}\n\0";
+
+	// Utwórz obiekt Vertex Shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// Połącz istniejący obiekt z napisaną wcześniej implementacją shadera
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// Skompiluj gotowy kod
+	glCompileShader(vertexShader);
+	// Powtórz operację dla fragment shadera
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Utwórz program
+	GLuint shaderProgram = glCreateProgram();
+	// Podłącz shadery pod program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Usuń niepotrzebne shadery
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Vertices coordinates
+	GLfloat vertices[] =
+	{
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
+
+	GLuint VAO, VBO;
+	// Utwórz obiekty VBO i VAO, każdy posiada jeden obiekt
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// Połącz wierzchołki z bufforem wierzchołków
+	glBindVertexArray(VAO);
+	// Ustaw typ VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// załaduj przygotowane wierzchołki
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+		GL_STATIC_DRAW);
+	// Skonfiguruj format buffora, typ danych i długość
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 *
+		sizeof(float), (void*)0);
+	// Uruchom buffor
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Ustaw kolor tła (RGBA, z przedziału <0, 1>)
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Wyczyść buffor I nadaj mu kolor
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Wybierz, który shader będzie używany
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		// Narysuj trójkąt
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Odśwież widok
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+
+
+
+
+	return 0;
+}
+
