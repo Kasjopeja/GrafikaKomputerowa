8c837e6ad01693fd26c8e3601aed85bf9f660623
Aleksandra <azakrecka@student.agh.edu.pl>
2024-03-23 22:47:14 +0100

    trojkaty

---
+++ Grafika 15.02.2024 part 2/main.cpp
@@ -0,0 +1,164 @@
+﻿#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+using namespace std;
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"in vec2 position;\n"
+"in vec3 color;\n"
+"out vec3 Color;\n"
+"void main()\n"
+"{\n"
+" Color = color;\n"
+" gl_Position = vec4(position, 0.0, 1.0);\n"
+"}\0";
+
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"in vec3 Color;\n"
+"out vec4 outColor;\n"	
+"void main()\n"
+"{\n"
+" outColor = vec4(Color, 1.0);\n"
+"}\n\0";
+
+int main() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Openglwindow", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" <<
+			std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+
+
+	GLuint VAO;
+
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+
+	// Vertices coordinates
+	GLfloat vertices[] =
+	{
+	-0.5f, 0.5f, 1.0f, 0.0f, 0.0f, // Lower left corner
+	0.5f, 0.5f, 0.0f, 1.0f, 0.0f, // Lower right corner
+	0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Upper right corner
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f // Upper left corner
+	};
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// za�aduj przygotowane wierzcho�ki
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint ebo;
+	glGenBuffers(1, &ebo);
+	GLuint elements[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint vStatus;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vStatus);
+	cout << "Status kompilacji Vertex Shadera: ";
+	if (vStatus == GL_TRUE) cout << "RIGHT COMPILATION" << endl << endl;
+	else {
+		cout << "BAD COMPILATION" << endl;
+		char vBuffer[128];
+		glGetShaderInfoLog(vertexShader, 64, NULL, vBuffer);
+		cout << "Log Kompilacji Vertex Shadera: " << endl << vBuffer << endl << endl;
+	}
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+
+
+
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	glUseProgram(shaderProgram);
+
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+
+
+	// Specify the color of the background
+	glClearColor(0.01f, 0.01f, 0.01f, 0.01f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Ustaw kolor t�a (RGBA, z przedzia�u <0, 1>)
+		glClearColor(0.0f, 0.0f, 0.0f, 0.1f);
+		// Wyczy�� buffor I nadaj mu kolor
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * sizeof(GL_UNSIGNED_INT)));
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+
+
+	return 0;
+}
--- Grafika 15.03.2024 wariant własny/main.cpp
+++ Grafika 15.03.2024 wariant własny/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -42,7 +44,6 @@ int main()
 	// Vertex Shader source code
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
-		"uniform float xOffset;\n"
 		"void main()\n"
 		"{\n"
 		" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
@@ -75,26 +76,70 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// Define vertices for one strip made of two triangles (a quad)
-	GLfloat vertices[] = {
-		// First triangle
-		-0.1f,  0.6f, 0.0f, // Top
-		 0.0f,  0.4f, 0.0f, // Bottom Left
-		 0.1f,  0.6f, 0.0f, // Bottom Right
-	};
+	// Vertices coordinates
+	vector <GLfloat> vertices;
+	
+	for (int j = 0; j < 3; j++)
+	{
+		for (int i = 0; i < 11; i++)
+		{
+			vertices.push_back(-1.0 + (0.8 * j) + (((i + 1) % 2) * 0.4));
+			vertices.push_back(1.0 - (0.4 * ceil(double(i) / 2)));
+		}
+	}
 
-	GLuint VAO, VBO;
+	for (int i = 0; i < vertices.size(); i = i + 2)
+	{
+		cout << vertices[i] << " " << vertices[i + 1] << endl;
+	}
+
+	// Indices for vertices order
+	vector <GLuint> indices = { 0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 13, 14, 15, 15, 16, 17, 17, 18, 19, 19, 20, 21, 22, 23, 24, 24, 25, 26, 26, 27, 28, 28, 29, 30, 30, 31, 32};
+
+	vector<GLuint> sequence;
+
+	for (int i = 0; i <= 32; i++) {
+		if (i % 4 == 0) {
+			// Every fourth number (starting from 0) is not repeated
+			// It's the beginning of a 'zag'
+			sequence.push_back(i);
+		}
+		else if ((i + 1) % 4 == 0) {
+			// Every fourth number (offset by 1) is the end of a 'zig'
+			// and is not repeated
+			sequence.push_back(i);
+			sequence.push_back(i + 1); // Begin next 'zag'
+		}
+		else {
+			// Middle number of a 'zig' or first number of a 'zag'
+			sequence.push_back(i);
+			sequence.push_back(i);
+		}
+	}
+
+	// Output the sequence
+	for (int num : sequence) {
+		cout << num << ", ";
+	}
+	cout << endl;
+
+	GLsizei elemSize = indices.size() * sizeof(GLuint);
+
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -102,32 +147,21 @@ int main()
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+
 	while (!glfwWindowShouldClose(window))
 	{
-		// Clear the screen
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Use the shader program
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-
-		// Draw the triangles in a vertical stripe pattern
-		for (float x = -1.0f; x <= 1.0f; x += 0.2f) {
-			// Set the offset for the current position
-			glUniform1f(glGetUniformLocation(shaderProgram, "xOffset"), x);
-
-			// Draw the triangle
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-		}
-
-		// Swap buffers and poll for IO events
+		glDrawElements(GL_TRIANGLES, elemSize, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	// Delete window before ending the program
@@ -138,4 +172,3 @@ int main()
 
 	return 0;
 }
-
